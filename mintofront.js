ea5fdb0306fbf70560efe14a98d136034ea3fdf5
ricardoleonh <67030936+ricardoleonh@users.noreply.github.com>
2020-11-02 07:51:32 -0600

    Add files via upload

---
+++ mintofront.js
@@ -0,0 +1,17 @@
+function minToFront(arr) {
+    var min = arr[0];
+    var idx = 0;
+    for (var i = 1; i < arr.length; i++){
+        if (arr[i] < min) {
+            min = arr[i];
+            idx = i;
+        }
+    }
+    for (var x = idx; x > 0; x--){
+        var temp = arr[x];
+        arr[x] = arr[x-1];
+        arr[x-1] = temp;
+    }
+    return arr;
+}
+console.log(minToFront([4,2,1,3,5]))
